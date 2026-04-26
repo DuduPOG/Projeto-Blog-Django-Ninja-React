bb4b6c3b6f2e7b20277f7265f6c7bc8e1388ddbf
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-26 11:39:52 -0300

    feat: criando esboço da aplicação de post

---
+++ backend/blog/post/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+usuario:
+nome
+username
+email
+senha
+
+post:
+titulo
+descricao
+imagem
+data_publicacao
+usuario
+
+comentario:
+
+resposta:
+
+resposta_comentario:
+"""
